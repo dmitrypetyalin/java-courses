1d6166202125ba77357607b909183aa37bf55df0
Dmitry Petyalin <dmitry.petyalin@gmail.com>
2017-08-23 01:39:23 +0700

    Delete Calculate.java

--- Calculate.java
+++
@@ -1,8 +0,0 @@
-public class Calculate {
-	public static void main(String[] args){
-		int x = Integer.parseInt(args[0]);
-		int y = Integer.parseInt(args[1]);
-		int result = x + y;
-		System.out.println("Addition is " + result);
-	}
-}
